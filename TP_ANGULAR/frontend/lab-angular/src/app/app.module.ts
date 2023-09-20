11ff82166e9eb0a48464d213a6cd20a1a39d6f88
AdrianAguero <adri.11.10.96@gmail.com>
2023-09-20 16:47:14 -0300

    entrega del ultimo tp

---
+++ TP_ANGULAR/frontend/lab-angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ClientesModule } from './modules/clientes/clientes.module';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ClientesModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
